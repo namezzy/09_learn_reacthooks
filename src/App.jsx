a96e8d4819dd1ca51a53519c8905c0b840fdd81e
withLevi <zengyin.ssr@gmail.com>
2024-05-08 17:19:20 +0800

    Initial react project and reduce useless code

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import React, { PureComponent } from 'react'
+
+export class App extends PureComponent {
+  render() {
+    return (
+      <div>
+        <h1>App page</h1>
+      </div>
+    )
+  }
+}
+
+export default App
