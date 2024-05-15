7a86ff67eff5b091271e5262a1dd668ac8e173a1
withLevi <zengyin.ssr@gmail.com>
2024-05-15 17:01:08 +0800

    Manage complex data by useReducer

--- src/index.js
+++ src/index.js
@@ -5,7 +5,8 @@ import { UserContext, ThemeContext } from "./05_useContext的使用/context";
 // import App from "./02_计数器实现对比/App";
 // import App from "./03_useState的使用/App";
 // import App from "./04_useEffect的使用/App";
-import App from "./05_useContext的使用/App";
+// import App from "./05_useContext的使用/App";
+import App from "./06_useReducer的使用(了解)/App";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
