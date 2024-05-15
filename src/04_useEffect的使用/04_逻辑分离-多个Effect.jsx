b86c0f073c819e309e2633a854d217cd4cebfbef
withLevi <zengyin.ssr@gmail.com>
2024-05-15 15:38:27 +0800

    Add excution timing -controlling callback execution.

--- src/04_useEffect的使用/04_逻辑分离-多个Effect.jsx
+++ src/04_useEffect的使用/04_逻辑分离-多个Effect.jsx
@@ -4,46 +4,41 @@ import { useEffect } from "react";
 
 const App = memo(() => {
   const [count, setCount] = useState(0);
-  const [message, setMessage] = useState("Hello World.")
 
-
+  // 负责告知react， 在执行完成当前组件渲染之后要执行的副作用代码
   useEffect(() => {
+    // 1. 修改document的title(1行)
+    console.log("修改title")
 
-    console.log("修改title", count)
-
-  }, [count]);
+  });
 
 
-
+  // 一个函数式组件中，可以存在多个useEffect
   useEffect(() => {
     console.log("监听redux中的数据")
 
+    // 2. 对redux中数据变化监听
 
+    // 3. 监听eventBus中的why事件
 
     return () => {
-
+      // 取消redux中数据的监听
     }
 
-  }, [])
+  })
 
   useEffect(() => {
-
+    // 3.监听eventBus中的why事件
     console.log("监听eventBus的why事件")
 
     return () => {
-
+      // 取消eventBus中why事件的监听
     }
-  }, [])
-
-  useEffect( () => {
-
-    console.log("发送网络请求， 从服务器获取数据")
-  }, [])
+  })
 
   return (
     <div>
       <button onClick={(e) => setCount(count + 1)}>+1({count})</button>
-      <button onClick={ e => setMessage("你好啊李银河")}>修改message({message})</button>
     </div>
   );
 });
