5449d4bca77a720207da67e1650b6169db957140
withLevi <zengyin.ssr@gmail.com>
2024-05-18 22:01:48 +0800

    Add useMemo.

---
+++ src/08_useMemo的使用/App.jsx
@@ -0,0 +1,13 @@
+import React, { memo } from 'react'
+import { useMemo, useState } from 'react'
+
+const App = memo(() => {
+    const [count, setCount ] = useState(0)
+  return (
+    <div>
+        <h2>计算结果: </h2>
+    </div>
+  )
+})
+
+export default App
